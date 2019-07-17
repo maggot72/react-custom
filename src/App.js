424a1e241c5f112ea023c66899663667cea934eb
ssy <gugudan727@gmail.com>
2019-07-17 21:04:15 +0900

    customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Customer from './components/Customer'
 
-class App extends Component {
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : 'hong gil dong',
+  'birthday' :'721214',
+  'gender' : 'male',
+  'job' : 'undergraduate'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : 'hong gil dong2',
+  'birthday' :'821214',
+  'gender' : 'male',
+  'job' : 'lawyer'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : 'hong gil dong3',
+  'birthday' :'921214',
+  'gender' : 'female',
+  'job' : 'doctor'
+}
+]
+
+class App extends React.Component {
+
   render(){
     return (
-      <div className="gray-background">
-         <img src={logo} lat="logo" />
-         <h2>custom system</h2>
+      <div>
+         {
+           customers.map(c => {
+             return (
+               <Customer 
+                 key={c.id}
+                 id={c.id}
+                 name={c.name}
+                 image={c.image}
+                 birthday={c.birthday}
+                 gender={c.gender}
+                 job={c.job}
+               />
+             );
+           })
+         }
       </div>
     );
   }
