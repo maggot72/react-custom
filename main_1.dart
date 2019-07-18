6640c573b57a9101b797618ae89ef1e1c49ec256
maggot72 <48583371+maggot72@users.noreply.github.com>
2019-07-18 15:48:49 +0900

    Create main_1.dart

---
+++ main_1.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       primarySwatch: Colors.blue,
+      ),
+      home: HelloPage("Hello World")
+    );
+  }
+}
+
+class HelloPage extends StatefulWidget {
+  final String title;
+
+
+  HelloPage(this.title) : super();
+
+  @override
+  _HelloPageState createState() => _HelloPageState();
+}
+
+class _HelloPageState extends State<HelloPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Text(widget.title,style: TextStyle(fontSize: 30)));
+  }
+}
