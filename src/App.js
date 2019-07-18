b3071ae6f3d06e830a569e3cb1de6863c7bb0fdd
ssy <gugudan727@gmail.com>
2019-07-18 16:48:02 +0900

    20190718-1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import './App.css';
 import Customer from './components/Customer'
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import Paper from '@material-ui/core/Paper';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit*3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 
 const customers = [
   {
@@ -32,8 +50,21 @@ const customers = [
 class App extends React.Component {
 
   render(){
+    const { classes} = this.props;
     return (
-      <div>
+      <Paper className = {classes.root}>
+         <Table className = {classes.table}>
+           <TableHead>
+             <TableRow>
+               <TableCell>번호</TableCell>       
+               <TableCell>이미지</TableCell>    
+               <TableCell>이름</TableCell>    
+               <TableCell>생일</TableCell>    
+               <TableCell>성별</TableCell>    
+               <TableCell>직업</TableCell>      
+             </TableRow>
+           </TableHead>
+           <TableBody>
          {
            customers.map(c => {
              return (
@@ -49,9 +80,11 @@ class App extends React.Component {
              );
            })
          }
-      </div>
+           </TableBody>
+         </Table>
+      </Paper>
     );
   }
  }
 
-export default App;
+export default withStyles(styles)(App);
