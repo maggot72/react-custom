b7871e5a039ea456b7f611fc0dd071b0ad07e2bc
ssy <gugudan727@gmail.com>
2019-07-20 15:08:33 +0900

    20190720-1

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableBody from '@material-ui/core/TableBody';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Paper from '@material-ui/core/Paper';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -17,37 +18,38 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing.unit * 2
   }
 });
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : 'hong gil dong',
-  'birthday' :'721214',
-  'gender' : 'male',
-  'job' : 'undergraduate'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : 'hong gil dong2',
-  'birthday' :'821214',
-  'gender' : 'male',
-  'job' : 'lawyer'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : 'hong gil dong3',
-  'birthday' :'921214',
-  'gender' : 'female',
-  'job' : 'doctor'
-}
-]
+
 
 class App extends React.Component {
+
+  state = {
+    customers: '',
+    completed: 0
+  }
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
+  //  this.callApi()
+  //    .then(res => this.setState({customers: res}))
+  //    .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
+  };
 
   render(){
     const { classes} = this.props;
@@ -66,7 +68,7 @@ class App extends React.Component {
            </TableHead>
            <TableBody>
          {
-           customers.map(c => {
+           this.state.customers? this.state.customers.map(c => {
              return (
                <Customer 
                  key={c.id}
@@ -76,9 +78,13 @@ class App extends React.Component {
                  birthday={c.birthday}
                  gender={c.gender}
                  job={c.job}
-               />
-             );
-           })
+               />);
+           }) : 
+           <TableRow>
+                  <TableCell colSpan="6" align="center">
+                    <CircularProgress className={classes.progress} variant="determinate" value={this.state.completed} />
+                  </TableCell>
+            </TableRow>
          }
            </TableBody>
          </Table>
--- server.js
+++ server.js
@@ -7,8 +7,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'hello express'});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id' : 1,
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : 'hong gil dong',
+        'birthday' :'721214',
+        'gender' : 'male',
+        'job' : 'undergraduate'
+      },
+      {
+        'id' : 2,
+        'image' : 'https://placeimg.com/64/64/2',
+        'name' : 'hong gil dong2',
+        'birthday' :'821214',
+        'gender' : 'male',
+        'job' : 'lawyer'
+      },
+      {
+        'id' : 3,
+        'image' : 'https://placeimg.com/64/64/3',
+        'name' : 'hong gil dong3',
+        'birthday' :'921214',
+        'gender' : 'female',
+        'job' : 'doctor'
+      }
+      ]
+   );
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
